dc593b20ae3f4353e827d7295f06121b0bacb47c
geniusjun <geniusjun4663@gmail.com>
2025-10-15 20:44:27 +0900

    feat(CustomException): 커스텀 예외 처리 클래스 생성
    IllegalArgumentException을 가독성 있게 처리하여 다양한 에러를 잡기 위해서 커스텀 예외 클래스 를 구현했다.

---
+++ src/main/java/calculator/global/CustomException.java
@@ -0,0 +1,14 @@
+package calculator.global;
+
+public class CustomException extends IllegalArgumentException {
+
+    private CustomException(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+    }
+
+    public static CustomException from(ErrorMessage errorMessage) {
+        return new CustomException(errorMessage);
+    }
+
+
+}
