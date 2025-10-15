105126630126330faa9b7937e10babd134f49740
geniusjun <geniusjun4663@gmail.com>
2025-10-15 21:18:57 +0900

    feat(CustomException): 에러 메세지의 접두사에 [ERROR]를 자동으로 추가하도록 설정
    CustomException 클래스에서 에러 메시지를 오류 클래스에 전달할 때 PREFIX를 함께 전달하도록 설정했다.

--- src/main/java/calculator/global/CustomException.java
+++ src/main/java/calculator/global/CustomException.java
@@ -1,9 +1,10 @@
 package calculator.global;
 
 public class CustomException extends IllegalArgumentException {
+    private static final String PREFIX = "[ERROR]";
 
     private CustomException(ErrorMessage errorMessage) {
-        super(errorMessage.getMessage());
+        super(PREFIX + errorMessage.getMessage());
     }
 
     public static CustomException from(ErrorMessage errorMessage) {
