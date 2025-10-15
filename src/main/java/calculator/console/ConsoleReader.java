eb6177a81df2c22e832d2e31f7136b73db611f9e
geniusjun <geniusjun4663@gmail.com>
2025-10-15 20:48:59 +0900

    feat(ConsoleReader): 콘솔 입력 단에서 빈 문자열을 검증하기 위한 정적 내부 클래스 생성
    빈 문자열의 경우 ConsoleReader 쪽에 책임이 있다고 생각하여, ConsoleReader로직을 구현했다. 앞으로도 계층간 예외는 이렇게 잡을 생각이다.

--- src/main/java/calculator/console/ConsoleReader.java
+++ src/main/java/calculator/console/ConsoleReader.java
@@ -1,9 +1,24 @@
 package calculator.console;
 
+import calculator.global.CustomException;
+import calculator.global.ErrorMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleReader {
     public static String readMessage() {
-        return Console.readLine();
+        return Validator.validate(Console.readLine());
+    }
+
+    private static class Validator {
+        public static String validate(String message) {
+            return validateBlank(message);
+        }
+
+        private static String validateBlank(String message) {
+            if (message.isBlank()) {
+                throw CustomException.from(ErrorMessage.BLANK_INPUT_ERROR);
+            }
+            return message;
+        }
     }
 }
