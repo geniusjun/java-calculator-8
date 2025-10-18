e203c54ab200645c0ea430300daea535689f7532
geniusjun <geniusjun4663@gmail.com>
2025-10-18 14:53:01 +0900

    feat(OutputView): 결과를 출력하는 기능
    실질적인 콘솔 출력은 ConsoleWriter가 담당하고 OutputView가 이를 호출하는 식으로 출력한다.

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,8 +4,13 @@ import calculator.console.ConsoleWriter;
 
 public class OutputView {
     private static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public void start() {
         ConsoleWriter.printlnMessage(START_MESSAGE);
+    }
+
+    public void result(int result) {
+        ConsoleWriter.printlnMessage(RESULT_MESSAGE + result);
     }
 }
