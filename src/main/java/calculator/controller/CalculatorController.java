bc72f663fbbf193c3d6e6cf589d43479d24e0f7b
geniusjun <geniusjun4663@gmail.com>
2025-11-28 13:01:08 +0900

    Feat(OutputView): 시작 안내 문구 출력 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,19 @@
 package calculator.controller;
 
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printStartMessage();
+    }
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,8 @@
 package calculator.view;
 
 public class OutputView {
+
+    public void printStartMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
 }
